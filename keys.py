fb917670b0c4fa7f3a0396228238c0090c4a5651
Henri Salonen <salonen.henri.tapani@gmail.com>
2018-06-07 19:59:06 +0300

    Second commit

---
+++ keys.py
@@ -0,0 +1,2 @@
+keys = dict(
+    screen_name = "your screen_name here", consumer_key = "your consumer_key here", consumer_secret = "your consumer_secret here", access_token = "your access_token here", access_token_secret = "your access_token_secret here")
